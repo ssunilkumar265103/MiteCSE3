4b8c4b7bbdaeeed7ab1f39e32b6499e9f1941d77
ssunilkumar <sunil@mite.ac.in>
2020-09-17 12:02:08 +0530

    adter adding firebase database retrieval msg for listview

--- app/src/main/java/com/example/mitecse3/ViewCircularMsg.java
+++ app/src/main/java/com/example/mitecse3/ViewCircularMsg.java
@@ -1,10 +1,17 @@
 package com.example.mitecse3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -13,13 +20,33 @@ public class ViewCircularMsg extends AppCompatActivity {
     private ArrayList<String> arrayList = new ArrayList<>();
     private ArrayAdapter<String> arrayAdapter;
     private ListView circular_msg_list;
+    private FirebaseDatabase db3;
+    private DatabaseReference myRef3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_circular_msg);
         circular_msg_list = (ListView) findViewById(R.id.Circular_msg);
+        db3 = FirebaseDatabase.getInstance();
+        myRef3 = db3.getReference("Notices3");
         arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
         arrayList.add("test1");
         circular_msg_list.setAdapter(arrayAdapter);
+        myRef3.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot:dataSnapshot.getChildren()){
+                    arrayList.add(snapshot.getValue().toString());
+                }
+                arrayAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
